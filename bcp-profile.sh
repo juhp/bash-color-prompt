bf8b292600e570b7a8db56e4bc47a7bde2525014
Jens Petersen <petersen@redhat.com>
2026-01-06 23:51:49 +0800

    profile.d: add PS1 and TERM checks to control setup

--- bcp-profile.sh
+++ bcp-profile.sh
@@ -1,7 +1,8 @@
 # see /usr/share/doc/bash-color-prompt/README.md
 
 # only for bash
-if [ -n "${BASH_VERSION}" -a -z "${bash_color_prompt_disable}" -a -z "${bash_prompt_color_disable}" ]; then
+if [ -n "${BASH_VERSION}" -a -z "${bash_color_prompt_disable}" -a \
+        -z "${bash_prompt_color_disable}" ]; then
 
     # enable only in interactive shell
     case $- in
@@ -9,7 +10,15 @@ if [ -n "${BASH_VERSION}" -a -z "${bash_color_prompt_disable}" -a -z "${bash_pro
         *) if [ -z "${bash_prompt_color_test}" ]; then return; fi;;
     esac
 
-    source /usr/share/bash-color-prompt/bcp.sh
+    if [ '(' "$PS1" = "[\u@\h \W]\\$ " -o "$PS1" = "\\s-\\v\\\$ " -o \
+         "${TOOLBOX_PATH}" = "/usr/bin/toolbox" ')' -a \
+         '(' -n "${COLORTERM}" -o "${TERM: -5}" = "color" -o \
+         "${TERM}" = "linux" ')' -o -n "${bash_color_prompt_force}" -o \
+         -n "${bash_prompt_color_force}" ]; then
 
-    bcp_init
+        # TODO generate static PS1 at buildtime
+        source /usr/share/bash-color-prompt/bcp.sh
+
+        bcp_static
+    fi
 fi
